7d3c11d3d32d1c0397eafde31e19e296e0a1d74b
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2016-12-30 22:43:25 +0100

    Moved database configuration from application.yml to application.groovy to parse Heroku's environment variable.

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,36 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    dialect = org.hibernate.dialect.PostgreSQL9Dialect
+
+    uri = new URI(System.env.DATABASE_URL ?: "postgres://foodlog:logfood@cluster3/foodlog")
+    url = "jdbc:postgresql://" + uri.host + uri.path
+    username = uri.userInfo.split(":")[0]
+    password = uri.userInfo.split(":")[1]
+
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop"
+        }
+        hibernate {
+            show_sql = true
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "validate"
+        }
+        hibernate {
+            show_sql = true
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+        }
+    }
+}
