0b7347a77aec7c97415334a2671f19190f8624c9
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2016-12-31 01:57:38 +0100

    Insert a workout type as well for dev.

--- grails-app/init/se/tarlinder/foodlog/BootStrap.groovy
+++ grails-app/init/se/tarlinder/foodlog/BootStrap.groovy
@@ -3,6 +3,7 @@ package se.tarlinder.foodlog
 import grails.util.GrailsUtil
 import se.tarlinder.foodlog.domain.Food
 import se.tarlinder.foodlog.domain.User
+import se.tarlinder.foodlog.domain.WorkoutType
 
 class BootStrap {
 
@@ -11,7 +12,7 @@ class BootStrap {
             new User(name: 'user', password: 'user').save()
             new User(name: 'admin', password: 'admin', admin: true, kcalPerDay: 2500.0).save()
             new Food(name: 'Keso Mini',kcal: 70, protein: 11, carbohydrate: 2.5, fat: 1.5, category: 'DAIRY', packSizeInGrams:250).save()
-      //      new WorkoutType(name: 'Bodypump', averageKcal: 600).save()
+            new WorkoutType(name: 'Bodypump', averageKcal: 600).save()
         }
     }
 
