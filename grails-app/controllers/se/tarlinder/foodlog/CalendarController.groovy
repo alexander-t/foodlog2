b4e44deb36dee53bd6c3402cf60373c347d087ac
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2017-01-01 11:12:15 +0100

    Changed backing date implementation to the Java 8 equivalent.

--- grails-app/controllers/se/tarlinder/foodlog/CalendarController.groovy
+++ grails-app/controllers/se/tarlinder/foodlog/CalendarController.groovy
@@ -2,9 +2,12 @@ package se.tarlinder.foodlog
 
 import se.tarlinder.foodlog.model.Month
 
+import java.time.LocalDate
+import java.time.ZoneId
+
 class CalendarController {
 
     def index() {
-        [month: new Month(Calendar.getInstance(TimeZone.getTimeZone("GMT+1")))]
+        [month: new Month(LocalDate.now(ZoneId.of("GMT+1")))]
     }
 }
--- src/main/groovy/se/tarlinder/foodlog/model/Month.groovy
+++ src/main/groovy/se/tarlinder/foodlog/model/Month.groovy
@@ -1,26 +1,25 @@
 package se.tarlinder.foodlog.model
 
-import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 
 class Month {
 
-    private Calendar calendar
+    private static final SV_SE = new Locale("sv", "SE")
+    private LocalDate date
 
-    Month(Calendar calendar) {
-        this.calendar = calendar
+    Month(LocalDate date) {
+        this.date = date
     }
 
-    def name() {
-        SimpleDateFormat sdf = new SimpleDateFormat("MMMM", new Locale("sv", "SE"));
-        sdf.format(calendar.getTime()).capitalize()
+    String name() {
+        date.format(DateTimeFormatter.ofPattern("MMMM", SV_SE)).capitalize()
     }
 
     def dates() {
-
-        def dayToSimpleDate = []
-        for (day in 1..calendar.getMaximum(Calendar.DAY_OF_MONTH)) {
-            dayToSimpleDate << String.format('%1$4d%2$02d%3$02d', calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, day)
+        (1..YearMonth.of(date.year, date.month).lengthOfMonth()).collect {
+            String.format('%1$4d%2$02d%3$02d', date.year, date.month.value, it)
         }
-        return dayToSimpleDate
     }
 }
