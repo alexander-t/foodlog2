0b0f984b468509fbc057572faad2bc5c812cd2f5
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2017-01-08 23:53:09 +0100

    Ugly but working presentation of total stats.

--- grails-app/controllers/se/tarlinder/foodlog/StatsController.groovy
+++ grails-app/controllers/se/tarlinder/foodlog/StatsController.groovy
@@ -1,6 +1,18 @@
 package se.tarlinder.foodlog
 
+import groovy.sql.Sql
+import org.postgresql.util.PGobject
+
 class StatsController {
 
-    def index() { }
+    def index() {
+        Sql sql = new Sql(grailsApplication.mainContext.getBean('dataSource'))
+        def rows = sql.rows("select compute_total_stats()")
+        String unparsedRecord = ((PGobject) rows[0]["compute_total_stats"]).value.replaceAll(/[^\d,]/, "")
+        def (energyBalanceKcal, foodKcal, workoutKcal) = unparsedRecord.split(/,/, -1)
+
+        return [energyBalanceKcal: Integer.parseInt(energyBalanceKcal),
+                foodKcal         : foodKcal?.trim() ? Integer.parseInt(foodKcal) : 0,
+                workoutKcal      : workoutKcal?.trim() ? Integer.parseInt(workoutKcal) : 0]
+    }
 }
--- grails-app/views/stats/index.gsp
+++ grails-app/views/stats/index.gsp
@@ -12,6 +12,13 @@
 
 <body>
 <h2>Statistik och Data</h2>
+<h4>Energi in</h4>
+<p>Mat: ${foodKcal}</p>
+<h4>Energi ut</h4>
+<p>Energibalans: ${energyBalanceKcal}</p>
+<p>Träning: ${workoutKcal}</p>
+<h4>Totalt</h4>
+<p>${foodKcal - energyBalanceKcal - workoutKcal}</p>
 </body>
 
 </html>
